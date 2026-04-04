bc2295258d2f2001b98c1cd279a2dec61d6e4b50
guinetik <guinetik@gmail.com>
2026-04-04 02:20:09 -0300

    feat(map): add MapView and /map route

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/map',
+      name: 'map',
+      component: () => import('@/views/MapView.vue'),
+    },
   ],
 })
 
---
+++ src/views/MapView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import { MapViewController } from './MapViewController'
+
+const container = ref<HTMLElement>()
+const viewController = new MapViewController()
+
+onMounted(async () => {
+  if (container.value) {
+    await viewController.init(container.value)
+  }
+})
+
+onUnmounted(() => {
+  viewController.dispose()
+})
+</script>
+
+<template>
+  <div ref="container" class="scene-container"></div>
+</template>
