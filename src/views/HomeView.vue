fe133ac883aced4b4f84e620412e676d193bd08e
guinetik <guinetik@gmail.com>
2026-04-04 04:12:15 -0300

    feat(views): wire ShuttleHud into HomeView with full telemetry

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,31 @@
+<!-- src/views/HomeView.vue -->
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, reactive, onMounted, onUnmounted } from 'vue'
 import { HomeViewController } from './HomeViewController'
+import ShuttleHud from '@/components/ShuttleHud.vue'
+import type { ShuttleTelemetry } from '@/lib/ShuttleTelemetry'
 
 const container = ref<HTMLElement>()
 const viewController = new HomeViewController()
-const speed = ref(0)
-const heading = ref(0)
+const telemetry = reactive<ShuttleTelemetry>({
+  speed: 0,
+  heading: 0,
+  posX: 0,
+  posZ: 0,
+  fuelLevel: 0,
+  fuelCapacity: 0,
+  thrustCharge: 0,
+  thrustCapacity: 0,
+  brakeCharge: 0,
+  brakeCapacity: 0,
+  rcsCharge: 0,
+  rcsCapacity: 0,
+})
 
 onMounted(async () => {
   if (container.value) {
-    viewController.onTelemetry = (s, h) => {
-      speed.value = s
-      heading.value = h
+    viewController.onTelemetry = (t) => {
+      Object.assign(telemetry, t)
     }
     await viewController.init(container.value)
   }
@@ -20,17 +34,9 @@ onMounted(async () => {
 onUnmounted(() => {
   viewController.dispose()
 })
-
-function formatHeading(rad: number): string {
-  const deg = ((rad * 180) / Math.PI) % 360
-  return `${deg < 0 ? deg + 360 : deg}°`
-}
 </script>
 
 <template>
   <div ref="container" class="scene-container"></div>
-  <div class="hud">
-    <span>SPD {{ speed.toFixed(1) }}</span>
-    <span>HDG {{ formatHeading(heading) }}</span>
-  </div>
+  <ShuttleHud :telemetry="telemetry" />
 </template>
