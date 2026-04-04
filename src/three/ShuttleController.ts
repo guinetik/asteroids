efd707265cf45670a83cc263ddaeda6b7befdccc
guinetik <guinetik@gmail.com>
2026-04-04 02:00:28 -0300

    fix(three): lower nozzle Z positions significantly

--- src/three/ShuttleController.ts
+++ src/three/ShuttleController.ts
@@ -24,9 +24,9 @@ const MODEL_ROTATION_X = -Math.PI / 2
  * 3 SSME nozzles arranged in a triangle: one top-center, two bottom-sides.
  */
 const ENG_POSITIONS: [number, number, number][] = [
-  [-500, 0, 100],    // top center
-  [-500, -60, 40],   // bottom left
-  [-500, 60, 40],    // bottom right
+  [-500, 0, 30],     // top center
+  [-500, -60, -40],  // bottom left
+  [-500, 60, -40],   // bottom right
 ]
 
 const DOOR_OPEN_ANGLE = Math.PI * 0.6 // ~108 degrees, payload bay doors open wide
