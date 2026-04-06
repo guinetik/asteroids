8db6875551ef48dd6f1a72cf2ff6278f2d44f178
guinetik <guinetik@gmail.com>
2026-04-06 04:56:06 -0300

    feat(shop): add CreditsBadge HUD component

---
+++ src/components/hud/CreditsBadge.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  credits: number
+}>()
+</script>
+
+<template>
+  <div class="credits-badge">
+    <span class="credits-badge__icon">CR</span>
+    <span class="credits-badge__value">{{ credits.toLocaleString() }}</span>
+  </div>
+</template>
