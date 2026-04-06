9a6b44ccf0e19bc7213c31d6cc180508c2137d8c
guinetik <guinetik@gmail.com>
2026-04-06 04:56:05 -0300

    feat(shop): add ShopButton orbit HUD component

---
+++ src/components/shop/ShopButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  planetName: string
+}>()
+
+defineEmits<{
+  open: []
+}>()
+</script>
+
+<template>
+  <div class="shop-button-container">
+    <button
+      type="button"
+      class="shop-button"
+      @click="$emit('open')"
+    >
+      <span class="shop-button__label">Shop</span>
+      <span class="shop-button__planet">{{ planetName }}</span>
+    </button>
+    <span class="shop-button__hint">B</span>
+  </div>
+</template>
